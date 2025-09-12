406334c26138ad07a1148435a42a0740a74ac8bf
mikelugarterz <tiestomikel@gmail.com>
2025-09-11 18:32:33 -0600

    feat: initial config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/templates/screen-map/screen-map.component').then(m => m.ScreenMapComponent)
+    }
+];
---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  VITE_COUNTRIES_API_URL: 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets'
+};
